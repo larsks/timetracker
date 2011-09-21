cdf59a3f474e7b7ee0c5b307bce5b19e976902d4
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2011-09-21 15:42:49 -0400

    enabled sqlite foreign key support
    Enabled sqlite foreign key support as per:
    
    http://stackoverflow.com/questions/2614984/sqlite-sqlalchemy-how-to-enforce-foreign-keys
    
    This may cause problems if timetracker is used with a database
    other than sqlite.

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine, MetaData, \
         Column, Integer, Text, ForeignKey, \
         DateTime
 from sqlalchemy.sql.expression import *
+from sqlalchemy.interfaces import PoolListener
 
 Base = declarative_base()
 Session = None
@@ -15,21 +16,29 @@ class Project (Base):
     __tablename__ = 'projects'
     id = Column(Integer, primary_key=True)
     name = Column(Text(), unique=True)
-    work = relationship('Work', backref='project')
+    work = relationship('Work', backref='project', 
+            cascade='all,delete-orphan')
 
 class Work (Base):
     __tablename__ = 'worklog'
     id = Column(Integer, primary_key=True)
-    project_id = Column(Integer, ForeignKey('projects.id'))
+    project_id = Column(Integer, ForeignKey('projects.id',
+        ondelete='CASCADE'), nullable=False)
     comment = Column(Text())
     time_start = Column(DateTime(), server_default=text('current_timestamp'))
     time_stop = Column(DateTime())
+
+# From http://stackoverflow.com/questions/2614984/sqlite-sqlalchemy-how-to-enforce-foreign-keys
+# XXX: This may break with non-SQLite data sources.
+class ForeignKeysListener(PoolListener):
+    def connect(self, dbapi_con, con_record):
+        db_cursor = dbapi_con.execute('pragma foreign_keys=ON')
 
 def init(dburi):
     global Engine
     global Session
 
-    Engine = create_engine(dburi)
+    Engine = create_engine(dburi, listeners=[ForeignKeysListener()])
     Base.metadata.bind = Engine
     Base.metadata.create_all()
     Session = sessionmaker(bind=Engine)
