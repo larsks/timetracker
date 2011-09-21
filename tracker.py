e0e61cb6146b77d7ae23c443818ba348d9a0f917
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2011-09-21 14:16:11 -0400

    new commands and functionality
    - implemented cancel and log functions
    - more flexible reporting options
    - stoopid python date handling

--- tracker.py
+++ tracker.py
@@ -4,7 +4,9 @@ import os
 import sys
 import optparse
 import logging
+import time
 import datetime
+import pytz
 
 from sqlalchemy.sql.expression import *
 
@@ -15,12 +17,17 @@ class TrackerError (Exception):
 
 class Tracker (object):
 
+    def __init__ (self):
+
+        self.utcdelta = datetime.timedelta(
+                seconds=time.daylight and time.altzone or time.timezone)
+
     def parse_args(self):
         p = optparse.OptionParser()
         p.disable_interspersed_args()
         p.add_option('-d', '--dburi',
                 default=os.environ.get('TIMETRACKER_DBURI',
-                    os.path.join(os.environ['HOME'], '.timedb')))
+                    'sqlite:///%s' % os.path.join(os.environ['HOME'], '.timedb')))
         p.add_option('--debug', action='store_true')
 
         return p.parse_args()
@@ -56,11 +63,62 @@ class Tracker (object):
                 self.cmd_status(args)
             elif cmd == 'report':
                 self.cmd_report(args)
+            elif cmd == 'cancel':
+                self.cmd_cancel(args)
+            elif cmd == 'drop':
+                self.cmd_drop(args)
+            elif cmd == 'log':
+                self.cmd_log(args)
             else:
                 self.cmd_start([cmd] + args)
         except TrackerError, detail:
             self.log.error(detail)
             sys.exit(1)
+
+    def cmd_log(self, args):
+        try:
+            projname, time_start, time_stop = args[:3]
+            comment = ' '.join(args[3:])
+            project = self.get_project(projname)
+            self.log_work(project, time_start, time_stop, comment)
+        except ValueError:
+            raise TrackerError('You need to specify a project, start time, and stop time.')
+
+    def log_work(self, project, time_start, time_stop, comment):
+        now = datetime.datetime.now()
+
+        time_start = datetime.datetime.strptime(time_start, '%H:%M')
+        time_stop = datetime.datetime.strptime(time_stop, '%H:%M')
+
+        time_start = datetime.datetime.combine(
+                now, time_start.time()) + self.utcdelta
+        time_stop = datetime.datetime.combine(
+                now, time_stop.time()) + self.utcdelta
+
+        w = model.Work(project=project,
+                time_start=time_start, time_stop=time_stop,
+                comment=comment)
+
+        if comment:
+            comment = ' (%s)' % comment
+
+        self.session.add(w)
+        self.session.commit()
+
+        self.log.info('Logged %s work to %s%s.',
+                w.time_stop - w.time_start, project.name, comment)
+
+    def cmd_drop(self, args):
+        try:
+            projname = args.pop(0)
+            project = self.get_project(projname, create=False)
+
+            if project:
+                self.drop_project(project)
+            else:
+                raise TrackerError('No project named "%s".' % projname)
+        except IndexError:
+            raise TrackerError('No project?')
 
     def cmd_start(self, args):
         w = self.find_open_work()
@@ -78,6 +136,13 @@ class Tracker (object):
 
         self.start_work(project, args)
 
+    def cmd_cancel(self, args):
+        w = self.find_open_work()
+        if w:
+            self.cancel_work(w)
+        else:
+            raise TrackerError("You're not doing anything right now!")
+
     def cmd_stop(self, args):
         w = self.find_open_work()
         if w:
@@ -90,34 +155,79 @@ class Tracker (object):
             print project.name
 
     def parse_report_args(self, args):
-        p = optparse.OptionParser()
+        p = optparse.OptionParser(usage='tt report [options] [projects]')
         p.add_option('--since')
-        p.add_option('--days', '-d')
-        p.add_option('--week', action='store_true')
-        p.add_option('--month', action='store_true')
-        p.add_option('--verbose', '-v', action='store_true')
+        p.add_option('--days')
+        p.add_option('--weeks')
+        p.add_option('--today', '--day', '-T', action='store_true')
+        p.add_option('--week', '-W', action='store_true')
+        p.add_option('--month', '-M', action='store_true')
+
+        p.add_option('--seconds', '-s', action='store_true')
 
         return p.parse_args(args)
 
+    def start_of_week(self):
+        '''Returns midnight of the most recent Monday
+        as a datetime object.'''
+
+        now = datetime.datetime.utcnow()
+        midnight = datetime.datetime.combine(now, 
+                datetime.time(0,0)) + self.utcdelta
+
+        delta = datetime.timedelta(days=midnight.weekday())
+
+        return midnight - delta
+
+    def start_of_month(self):
+        '''Returns midnight of the first day of the month
+        as a datetime object.'''
+
+        now = datetime.datetime.utcnow()
+        midnight = datetime.datetime.combine(now, 
+                datetime.time(0,0)) + self.utcdelta
+
+        delta = datetime.timedelta(days=midnight.day - 1)
+
+        return midnight - delta
+
     def cmd_report(self, args):
         now = datetime.datetime.utcnow()
+        midnight = datetime.datetime.combine(now, 
+                datetime.time(0,0)) + self.utcdelta
 
         opts, args = self.parse_report_args(args)
+        projects = self.session.query(model.Project)
         
         if args:
-            projects = self.session.query(model.Project)\
-                    .filter(model.Project.name.in_(args))
+            projects = projects.filter(model.Project.name.in_(args))
+
+        if opts.days:
+            since = midnight - datetime.timedelta(days=int(opts.days))
+        elif opts.weeks:
+            since = midnight - datetime.timedelta(days=7 * int(opts.weeks))
+        elif opts.today:
+            since = midnight
+        elif opts.week:
+            since = self.start_of_week()
         else:
-            projects = self.session.query(model.Project).all()
+            since = None
 
-        for p in projects:
-            work = datetime.timedelta()
+        for project in projects:
+            work = self.session.query(model.Work)\
+                    .filter(model.Work.project==project)\
+                    .filter(model.Work.time_stop != None)
+
+            if since:
+                work = work.filter(model.Work.time_start > since)
+
+            acc = sum((w.time_stop - w.time_start for w in work),
+                    datetime.timedelta())
             
-            for x in (w.time_stop - w.time_start for w in self.session.query(model.Work)\
-                    .filter(model.Work.time_stop != None).filter(model.Work.project == p)):
-                work += x
+            if opts.seconds:
+                acc = acc.seconds
 
-            print '%-20s %s' % (p.name, work)
+            print '%-20s %s' % (project.name, acc)
 
     def cmd_status(self, args):
         now = datetime.datetime.utcnow()
@@ -139,6 +249,11 @@ class Tracker (object):
                     w.comment and ' (%s)' % w.comment or '',
                     w.time_stop - w.time_start)
 
+    def drop_project(self, project):
+        self.log.info('Dropping project "%s".' % project.name)
+        self.session.delete(project)
+        self.session.commit()
+
     def start_work(self, project, args):
         self.log.info('Starting work on "%s".' % project.name)
         comment = ' '.join(args)
@@ -147,6 +262,15 @@ class Tracker (object):
         self.session.commit()
 
         return w
+
+    def cancel_work(self, w):
+        project = w.project
+        comment = w.comment and ' (%s)' % w.comment or ''
+        self.log.info('Canceling current work on %s%s.',
+                project.name, comment)
+
+        self.session.delete(w)
+        self.session.commit()
 
     def stop_work(self, w):
         project = w.project
@@ -171,11 +295,16 @@ class Tracker (object):
         return self.session.query(model.Work).filter(
                 model.Work.time_stop==None).first()
 
-    def get_project(self, projname):
+    def get_project(self, projname, create=True):
         project = self.session.query(model.Project).filter(
                 model.Project.name==projname).first()
 
-        return project and project or self.create_project(projname)
+        if project:
+            return project
+        elif create:
+            return self.create_project(projname)
+        else:
+            return None
 
     def create_project(self, projname):
         self.log.info('Creating project "%s".' % projname)
